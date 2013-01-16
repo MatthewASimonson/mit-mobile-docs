ebfd153ca2699a40b3c0479f9d3b12d9a4267188
Blake Skinner <bskinner@mit.edu>
2013-01-16 11:31:05 -0500

    Fix the copywrite information!

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MIT Mobile API'
-copyright = u'2013, Blake Skinner'
+copyright = u'2013, MIT'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
